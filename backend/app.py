e3649cf958b04b204ba8a7466b62f7f1779fc4f0
Frankyllen <frankyllen103@gmail.com>
2026-06-02 13:38:14 -0300

    Feat: Criada a rota POST /predict para recebimento de arquivos de imagem

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,30 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
+
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"status": "API do Frankyllen rodando com sucesso"})
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if "image" not in request.files:
+        return jsonify({"erro": "Nenhuma imagem foi enviada"}), 400
+    
+    arquivo_imagem = request.files["image"]
+    
+    if arquivo_imagem.filename == "":
+        return jsonify({"erro": "Arquivo de imagem inválido"}), 400
+
+
+    return jsonify({
+        "resultado": "Carro",
+        "confianca": 0.95
+    })
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
